8ce731c355577d282ee4b2bf356e2dce053f1bd1
ChrisWarner-Sage <chris.warner@sage.com>
2016-12-06 13:33:15 +0000

    1.1 not working example

---
+++ src/AspNetCoreCertError/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AspNetCoreCertError.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                var cert = store.Certificates.Find(X509FindType.FindBySubjectName, "localhost", false)[0];
+            }
+
+
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
